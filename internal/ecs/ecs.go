066ab4ab833db6f4303373cafb0ff5dc9a8b0ded
robshape <robshape@users.noreply.github.com>
2023-12-06 21:14:24 +0100

    test: Add more performance benchmark test

--- internal/ecs/ecs.go
+++ internal/ecs/ecs.go
@@ -1,30 +1,30 @@
 package ecs
 
-type ecs struct {
+type ecsCoordinator struct {
 	componentManager *componentManager
 	entityManager    *entityManager
 }
 
-func NewECS(maxEntities int) *ecs {
-	return &ecs{
+func NewECSCoordinator(maxEntities int) *ecsCoordinator {
+	return &ecsCoordinator{
 		componentManager: NewComponentManager(maxEntities),
 		entityManager:    NewEntityManager(maxEntities),
 	}
 }
 
-func (ecs *ecs) AddComponent(entity Entity, component component) error {
-	return ecs.componentManager.AddComponent(entity, component)
+func (ec *ecsCoordinator) AddComponent(entity Entity, component component) error {
+	return ec.componentManager.AddComponent(entity, component)
 }
 
-func (ecs *ecs) CreateEntity() (Entity, error) {
-	return ecs.entityManager.CreateEntity()
+func (ec *ecsCoordinator) CreateEntity() (Entity, error) {
+	return ec.entityManager.CreateEntity()
 }
 
-func (ecs *ecs) DestroyEntity(entity Entity) {
-	ecs.componentManager.DestroyEntityComponents(entity)
-	ecs.entityManager.DestroyEntity(entity)
+func (ec *ecsCoordinator) DestroyEntity(entity Entity) {
+	ec.componentManager.DestroyEntityComponents(entity)
+	ec.entityManager.DestroyEntity(entity)
 }
 
-func (ecs *ecs) RemoveComponent(entity Entity, componentType ComponentType) {
-	ecs.componentManager.RemoveComponent(entity, componentType)
+func (ec *ecsCoordinator) RemoveComponent(entity Entity, componentType ComponentType) {
+	ec.componentManager.RemoveComponent(entity, componentType)
 }
--- internal/ecs/ecs_test.go
+++ internal/ecs/ecs_test.go
@@ -8,7 +8,10 @@ import (
 	"github.com/robshape/deck/internal/ecs"
 )
 
-var result ecs.Entity
+const (
+	componentBenchmark1 = 1
+	componentBenchmark2 = 2
+)
 
 type benchmarkComponent struct {
 	componentType ecs.ComponentType
@@ -17,6 +20,8 @@ type benchmarkComponent struct {
 func (bc *benchmarkComponent) Type() ecs.ComponentType {
 	return bc.componentType
 }
+
+var result ecs.Entity
 
 func BenchmarkCreateEntities(b *testing.B) {
 	var r ecs.Entity // Avoid inline optimization (https://100go.co/89-benchmarks/)
@@ -38,13 +43,99 @@ func BenchmarkCreateEntities(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				// Avoid observer effect (https://100go.co/89-benchmarks/)
 				b.StopTimer()
-				ecs := ecs.NewECS(c.in)
+				ecsCoordinator := ecs.NewECSCoordinator(c.in)
 				b.StartTimer()
 
 				for j := 0; j < c.in; j++ {
-					entity, _ := ecs.CreateEntity()
-					ecs.AddComponent(entity, &benchmarkComponent{componentType: 1})
-					ecs.AddComponent(entity, &benchmarkComponent{componentType: 2})
+					entity, _ := ecsCoordinator.CreateEntity()
+					ecsCoordinator.AddComponent(entity, &benchmarkComponent{componentType: componentBenchmark1})
+					ecsCoordinator.AddComponent(entity, &benchmarkComponent{componentType: componentBenchmark2})
+
+					r = entity
+				}
+			}
+		})
+	}
+
+	result = r
+}
+
+func BenchmarkDestroyEntities(b *testing.B) {
+	var r ecs.Entity
+
+	cases := []struct {
+		name string
+		in   int
+	}{
+		{"16 entities, 2 components", 16},
+		{"64 entities, 2 components", 64},
+		{"256 entities, 2 components", 256},
+		{"1024 entities, 2 components", 1024},
+		{"4096 entities, 2 components", 4096},
+	}
+
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				ecsCoordinator := ecs.NewECSCoordinator(c.in)
+				entities := []ecs.Entity{}
+				for j := 0; j < c.in; j++ {
+					entity, _ := ecsCoordinator.CreateEntity()
+					ecsCoordinator.AddComponent(entity, &benchmarkComponent{componentType: componentBenchmark1})
+					ecsCoordinator.AddComponent(entity, &benchmarkComponent{componentType: componentBenchmark2})
+					entities = append(entities, entity)
+				}
+				b.StartTimer()
+
+				for j := 0; j < c.in; j++ {
+					entity := entities[j]
+					ecsCoordinator.DestroyEntity(entity)
+
+					r = entity
+				}
+			}
+		})
+	}
+
+	result = r
+}
+
+// func BenchmarkGetComponent(b *testing.B) {}
+
+func BenchmarkRemoveAddComponent(b *testing.B) {
+	var r ecs.Entity
+
+	cases := []struct {
+		name string
+		in   int
+	}{
+		{"16 entities, 1 component", 16},
+		{"64 entities, 1 component", 64},
+		{"256 entities, 1 component", 256},
+		{"1024 entities, 1 component", 1024},
+		{"4096 entities, 1 component", 4096},
+	}
+
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				b.StopTimer()
+				ecsCoordinator := ecs.NewECSCoordinator(c.in)
+				entities := []ecs.Entity{}
+				for j := 0; j < c.in; j++ {
+					entity, _ := ecsCoordinator.CreateEntity()
+					ecsCoordinator.AddComponent(entity, &benchmarkComponent{componentType: componentBenchmark1})
+					entities = append(entities, entity)
+				}
+				b.StartTimer()
+
+				for j := 0; j < c.in; j++ {
+					entity := entities[j]
+					ecsCoordinator.RemoveComponent(entity, componentBenchmark1)
+					ecsCoordinator.AddComponent(entity, &benchmarkComponent{componentType: componentBenchmark2})
 
 					r = entity
 				}
